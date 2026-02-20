d5ec6eebc839851f265fe33fc52a982cf96d453d
skyykc <xskywing5x@gmail.com>
2026-02-20 12:14:38 -0800

    [V0.2] timer and flag added; svg icons added

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ font = pygame.font.Font('font\helldiversfont.otf', 36)
 
 ### Game states
 STATE_MENU = 'menu'
+STATE_GAME = 'game'
 current_state = STATE_MENU
 
 ### Surfaces
@@ -22,6 +23,14 @@ current_state = STATE_MENU
 # Menu State
 title_surf = font.render('HELLDIVERS 2: STRATAGAME', True, 'White')
 title_rect = title_surf.get_rect(center=(400, 100))
+
+# Super Earth logo with color
+orig = pygame.image.load('graphics/logo_super_earth.png').convert_alpha()
+mask = pygame.mask.from_surface(orig)
+colored = mask.to_surface(setcolor=(hex(0x017191e)), unsetcolor=(0,0,0,0))
+
+super_earth_surf = pygame.transform.rotozoom(colored, 0, 0.02)
+super_earth_rect = super_earth_surf.get_rect(center=(400, 300))
 
 # Play button
 playtext_surf = font.render('START GAME', True, 'White')
@@ -36,10 +45,13 @@ playbutton_rect = playbutton_surf.get_rect(center=(400, 300))
 quittext_surf = font.render('QUIT', True, 'White')
 quittext_rect = quittext_surf.get_rect(center=(400, 400))
 
-quittext_color = 0x6e6e6e
+quitbutton_color = 0x6e6e6e
 quitbutton_surf = pygame.Surface((200, 80))
-quitbutton_surf.fill(quittext_color)
+quitbutton_surf.fill(quitbutton_color)
 quitbutton_rect = quitbutton_surf.get_rect(center=(400, 400))
+
+# Game State
+start_time = 0
 
 ### Main loop
 
@@ -47,47 +59,67 @@ running = True
 while running:
     dt = clock.tick(24) / 1000  # Delta time in seconds.
 
+    # process every event that has occurred this frame
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill('black')
+        if current_state == STATE_MENU:
+            # hover effects for both buttons
+            if event.type == pygame.MOUSEMOTION:
+                if playbutton_rect.collidepoint(event.pos):
+                    playbutton_color = 0x595959
+                    playbutton_surf.fill(playbutton_color)
+                else:
+                    playbutton_color = 0x6e6e6e
+                    playbutton_surf.fill(playbutton_color)
+
+                if quitbutton_rect.collidepoint(event.pos):
+                    quitbutton_color = 0x595959
+                    quitbutton_surf.fill(quitbutton_color)
+                else:
+                    quitbutton_color = 0x6e6e6e
+                    quitbutton_surf.fill(quitbutton_color)
+
+            # clicks
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if playbutton_rect.collidepoint(event.pos):
+                    playbutton_color = 0x2f2f2f
+                    playbutton_surf.fill(playbutton_color)
+                    current_state = STATE_GAME
+                    start_time = pygame.time.get_ticks()  # Time when the game state starts
+                elif quitbutton_rect.collidepoint(event.pos):
+                    running = False
+
+        elif current_state == STATE_GAME:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                current_state = STATE_MENU
+
+    # drawing happens after input processing
+    screen.fill(hex(0x12141a))
 
     if current_state == STATE_MENU:
+        # Draw menu
+        screen.blit(super_earth_surf, super_earth_rect)
         screen.blit(title_surf, title_rect)
         screen.blit(playbutton_surf, playbutton_rect)
         screen.blit(playtext_surf, playtext_rect)
         screen.blit(quitbutton_surf, quitbutton_rect)
         screen.blit(quittext_surf, quittext_rect)
-
-        # Play button hover and click effects
-        if event.type == pygame.MOUSEMOTION:
-            if playbutton_rect.collidepoint(event.pos):
-                playbutton_color = 0x595959
-                playbutton_surf.fill(playbutton_color)
-            else:
-                playbutton_color = 0x6e6e6e
-                playbutton_surf.fill(playbutton_color)
-    
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if playbutton_rect.collidepoint(mouse_pos):
-                playbutton_color = 'DarkGrey'
-                playbutton_surf.fill(playbutton_color)
-
-        # Quit button hover and click effects
-        if event.type == pygame.MOUSEMOTION:
-            if quitbutton_rect.collidepoint(event.pos):
-                quittext_color = 0x595959
-                quitbutton_surf.fill(quittext_color)
-            else:
-                quittext_color = 0x6e6e6e
-                quitbutton_surf.fill(quittext_color)
-    
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if quitbutton_rect.collidepoint(mouse_pos):
-                running = False
+    elif current_state == STATE_GAME:
+        # Draw game state
+        screen.blit(super_earth_surf, super_earth_rect)
+        current_time = (15000 - (pygame.time.get_ticks() - start_time))
+        if current_time > 10000:
+            score_surf = font.render(f'{current_time // 1000}', True, hex(0xffee00))
+            score_rect = score_surf.get_rect(center=(400, 100))
+        elif current_time >= 0:
+            score_surf = font.render(f'{current_time / 1000:.1f}', True, hex(0xff8d5d))
+            score_rect = score_surf.get_rect(center=(400, 100))
+        else:
+            score_surf = font.render('TIMES UP!', True, hex(0xff8d5d))
+            score_rect = score_surf.get_rect(center=(400, 100))
+        screen.blit(score_surf, score_rect)
 
     pygame.display.flip()
 
